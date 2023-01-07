3a8229d78513f2090786be536f6886a35844f63d
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-01-07 18:03:31 +0530

    initial commit
    Take multiple line input and store values in array

---
+++ index.ps1
@@ -0,0 +1,25 @@
+$arr = @()
+$arr1 = @()
+
+$input = ' ';
+$input1 = ' ';
+
+Write-host "Enter values :"
+
+while($input -ne "q"){
+    $input = Read-Host 
+    $arr += $input
+}
+
+Write-host "Enter values :"
+
+while($input1 -ne "q"){
+    $input1 = Read-Host 
+    $arr1 += $input1
+}
+
+for($i=0; $i -lt $arr.count-1 ; $i++){
+    for($j=0; $j -lt $arr1.count -1; $j++){
+Write-host "value1 : " $arr[$i] " " $arr1[$j]
+    }
+}
